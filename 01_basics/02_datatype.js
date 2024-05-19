7d6d82b1c8f06d4e4cc0f6a171f6ca5306b7c67e
Nisarg.  S. Bhoi <108231394+NISARG24TH@users.noreply.github.com>
2024-05-19 16:08:39 +0530

    Create 02_datatype.js

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // strict mode (treat all js code as newer version)
+
+// alert("3 + 3") // we are using nodejs, so alert is not supported But in browser it will work
+
+console.log(3 + 3);
+console.log("nisarg" + "bhoi");
+
+let name = "nisarg";
+let age = 23;
+let isLoggedIn = true;
+
+console.log(typeof name); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// number =>2 to 53
+//bigint => 2n
+//string => "nisarg"
+//boolean => true/false
+//NaN => Not a Number / standalone value
+//undefined => undefined
+//null => null
+//symbol => Symbol() / unique 
+
+
+//object => {key: value}
+//array => [1,2,3,4]
+//function => function() {}
