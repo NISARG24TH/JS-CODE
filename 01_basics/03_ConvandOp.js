3f696c60b535ce74d052d697fa891ec9bc208fb6
Nisarg.  S. Bhoi <108231394+NISARG24TH@users.noreply.github.com>
2024-05-19 18:05:51 +0530

    Update 03_ConvandOp.js

--- 01_basics/03_ConvandOp.js
+++ 01_basics/03_ConvandOp.js
@@ -1,7 +1,18 @@
 // 03_conversion&operation
-let score = "33";
-console.log(typeof(score)); //
+let score = "33abc"; // string
+console.log(typeof(score)); // AS A METHOD.
 console.log(typeof score);
 
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true  ==> 1; false ==> 0;
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+console.log(booleanIsLoggedIn)
+
+// 0, null, undefined, NaN, "" ==> false
+// 1, "abc", {}, [] ==> true
